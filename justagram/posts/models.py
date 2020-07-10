1ca90375845a6f71c070cdf0d10f1fe45c92b685
juheoung <juheoung0201@gmail.com>
2020-07-10 11:51:26 +0900

    update: add models

--- justagram/posts/models.py
+++ justagram/posts/models.py
@@ -1,2 +1,8 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+
+class Post(models.Model):
+    owner = models.ForeignKey('users.UserProfile', on_delete=models.CASCADE())
+    photo = ArrayField(models.ImageField())
+    contents = models.TextField()
