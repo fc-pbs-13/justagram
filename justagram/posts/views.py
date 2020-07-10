1d18b545ad350209585edd36345ff7a7796ee246
juheoung <juheoung0201@gmail.com>
2020-07-10 12:08:28 +0900

    update: Serializer, ViewSet

--- justagram/posts/views.py
+++ justagram/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
