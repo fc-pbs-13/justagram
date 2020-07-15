3d0e3c6d07ee734ae6666665a001659686ff49e7
juheoung <juheoung0201@gmail.com>
2020-07-15 14:58:19 +0900

    update: testcode

--- justagram/comments/tests.py
+++ justagram/comments/tests.py
@@ -54,7 +54,9 @@ class CommentTestCase(APITestCase):
     def test_list_comment(self):
         self.client.force_authenticate(user=self.user)
 
-        response = self.client.post()
+        response = self.client.get('/comment')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_comment(self):
         data = {
